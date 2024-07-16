a1802d8ce779d287103776a1c35aaeb96c2a9e93
Nia <nia@niaswift.com>
2024-07-16 18:32:34 +0300

    Custom Loot Tables and added Ruby Ore + variations of it

--- src/main/java/com/niaswift/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/niaswift/tutorialmod/block/ModBlocks.java
@@ -4,11 +4,13 @@ import com.niaswift.tutorialmod.TutorialMod;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
@@ -18,7 +20,17 @@ public class ModBlocks {
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
             new Block(AbstractBlock.Settings.copy(Blocks.RAW_IRON_BLOCK)));
 
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.copy(Blocks.STONE).strength(3.0F, 3.0F)));
 
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.copy(Blocks.DEEPSLATE).strength(4.5F, 3.0F)));
+
+    public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.copy(Blocks.NETHERRACK).strength(3.0F, 3.0F)));
+
+    public static final Block END_STONE_RUBY_ORE = registerBlock("end_stone_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 7), AbstractBlock.Settings.copy(Blocks.END_STONE).strength(4.0f, 4.5F)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
