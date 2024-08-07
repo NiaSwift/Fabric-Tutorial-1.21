b94f6fe07fc2da59005a43a058fe139845946aa8
Nia <nia@niaswift.com>
2024-08-07 12:24:08 +0300

    Added Tomato

--- src/main/java/com/niaswift/tutorialmod/item/ModItems.java
+++ src/main/java/com/niaswift/tutorialmod/item/ModItems.java
@@ -15,6 +15,8 @@ public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new Item.Settings()));
 
+    public static final Item TOMATO = registerItem("tomato", new Item(new Item.Settings().food(ModFoodComponents.TOMATO)));
+
     public static final Item METAL_DETECTOR = registerItem("metal_detector",
             new MetalDetectorItem(new Item.Settings().maxDamage(64)));
 
