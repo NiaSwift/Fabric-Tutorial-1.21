0728fbe241c01a941c25608d8b4093e85a7635b6
Nia <nia@niaswift.com>
2024-08-11 21:07:53 +0300

    Added tooltips for Metal Detector and Sound Block

--- src/main/java/com/niaswift/tutorialmod/block/custom/SoundBlock.java
+++ src/main/java/com/niaswift/tutorialmod/block/custom/SoundBlock.java
@@ -3,12 +3,18 @@ package com.niaswift.tutorialmod.block.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
 
@@ -21,5 +27,11 @@ public class SoundBlock extends Block {
                                  PlayerEntity player, BlockHitResult hit) {
         world.playSound(player, pos, SoundEvents.BLOCK_NOTE_BLOCK_XYLOPHONE.value(), SoundCategory.BLOCKS, 1f, 1f);
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.literal("Makes a sweet sound when right-clicked"));
+        super.appendTooltip(stack, context, tooltip, options);
     }
 }
